077eb252e84ac5185daca2f1ba20426f44338981
Kaylee Jones <kayleemaughan@gmail.com>
2016-03-08 04:45:31 +0000

    First commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.sendFile('weather.html', { root: 'public' });
+});
+
+router.get('/getcity',function(req,res,next) {
+  var myRe = new RegExp("^" + req.query.q); 
+  console.log("In getcity route");
+
+  fs.readFile(__dirname+'/cities.dat.txt',function(err,data) {
+    if(err) throw err;
+    var cities = data.toString().split("\n");
+    //console.log(myRe);
+    var jsonresult = [];
+    for(var i = 0; i < cities.length; i++) {
+      var result = cities[i].search(myRe);
+      if(result != -1) {
+	//console.log(cities[i]);
+	jsonresult.push({city:cities[i]});
+      }
+    }
+    res.status(200).json(jsonresult);
+    //console.log(jsonresult);
+  })
+});
+
+module.exports = router;
